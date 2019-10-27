d654b75d700391202bd232fb31df1da37ac4189b
Evgenya Sharapova <eva.sharapova1@mail.ru>
2019-10-27 13:20:41 +0300

    dz1

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,22 +1,23 @@
-let countLost = document.getElementsById("lost");
-let countDead = document.getElementsById("dead");
+let countLost = document.getElementById("lost");
+let countDead = document.getElementById("dead");
 let hole;
 for (let i = 1;i < 10; i++) {
 	hole = getHole(i);
 	hole.onclick = checkHit;
 }
-function checkHit(hole) {
+function checkHit() {
 	
-		if (hole.className.includes( 'hole_has-mole' )) {
-			countLost.textContent++;
-		} else {
+		if (hole.classList.contains('hole_has-mole')) {
 			countDead.textContent++;
+		} else {
+			countLost.textContent++;
 		}
-		if (countLost.textContent == 10) {
+		if (countDead.textContent == 10) {
 			alert("You win!");
 			countLost.textContent = 0;
 			countDead.textContent = 0;
-		} else{
+		}
+		if (countLost.textContent == 5) {
 			alert("Game over");
 			countLost.textContent = 0;
 			countDead.textContent = 0;
@@ -27,31 +28,31 @@ function checkHit(hole) {
 function getHole( index ) {
 	switch (index) {
 		case 1: 
-			hole = document.getElementsById("hole1");
+			hole = document.getElementById("hole1");
 			break;
 		case 2: 
-			hole = document.getElementsById("hole2");
+			hole = document.getElementById("hole2");
 			break;
 		case 3: 
-			hole = document.getElementsById("hole3");
+			hole = document.getElementById("hole3");
 			break;
 		case 4: 
-			hole = document.getElementsById("hole4");
+			hole = document.getElementById("hole4");
 			break;
 		case 5: 
-			hole = document.getElementsById("hole5");
+			hole = document.getElementById("hole5");
 			break;
 		case 6: 
-			hole = document.getElementsById("hole6");
+			hole = document.getElementById("hole6");
 			break;
 		case 7: 
-			hole = document.getElementsById("hole7");
+			hole = document.getElementById("hole7");
 			break;
 		case 8: 
-			hole = document.getElementsById("hole8");
+			hole = document.getElementById("hole8");
 			break;
 		case 9: 
-			hole = document.getElementsById("hole9");
+			hole = document.getElementById("hole9");
 			break;
 	}
 	return hole;
