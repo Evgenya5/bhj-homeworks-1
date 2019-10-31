bfcd92a274c9325dd72ccf6b7dec8766f04ad04a
Evgenya Sharapova <eva.sharapova1@mail.ru>
2019-10-31 22:50:49 +0300

    dz2 new

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,23 +1,22 @@
 let elementModalMain = document.getElementById("modal_main");
-let oldClassModalMain = elementModalMain.className;
-elementModalMain.className = `${oldClassModalMain} modal_active`
+elementModalMain.classList.add("modal_active");
 let elementsModalClose = document.getElementsByClassName("modal__close");
 let elementsShowSuccess = document.getElementsByClassName("show-success");
 let elementModalSuccess = document.getElementById("modal_success");
-let oldClassModalSuccess = elementModalSuccess.className;
 let arr = Array.from(elementsModalClose);
 let length = arr.length;
 for (let i = 0; i < length; i++) {
 	elementsModalClose[i].onclick = close;
 }
 function close() {
-	elementModalMain.className = `${oldClassModalMain}`;
-	elementModalSuccess.className = `${oldClassModalSuccess}`;
+	elementModalMain.classList.remove("modal_active");
+	elementModalSuccess.classList.remove("modal_active");
 };
 let arr1 = Array.from(elementsShowSuccess);
 let length1 = arr1.length;
 for (let i = 0; i < length1; i++) {
 	elementsShowSuccess[i].onclick = function() {
-		elementModalSuccess.className = `${oldClassModalSuccess} modal_active`;
+		elementModalMain.classList.remove("modal_active");
+		elementModalSuccess.classList.add("modal_active");
     };
 }
