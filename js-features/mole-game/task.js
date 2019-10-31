98c2d8eb6c205a71776a83f376163109e5dba03d
Evgenya Sharapova <eva.sharapova1@mail.ru>
2019-10-31 22:37:54 +0300

    dz 1 new

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -6,8 +6,7 @@ for (let i = 1;i < 10; i++) {
 	hole.onclick = checkHit;
 }
 function checkHit() {
-	
-		if (hole.classList.contains('hole_has-mole')) {
+		if (this.classList.contains('hole_has-mole')) {
 			countDead.textContent++;
 		} else {
 			countLost.textContent++;
@@ -25,36 +24,7 @@ function checkHit() {
 	
 }
 
-function getHole( index ) {
-	switch (index) {
-		case 1: 
-			hole = document.getElementById("hole1");
-			break;
-		case 2: 
-			hole = document.getElementById("hole2");
-			break;
-		case 3: 
-			hole = document.getElementById("hole3");
-			break;
-		case 4: 
-			hole = document.getElementById("hole4");
-			break;
-		case 5: 
-			hole = document.getElementById("hole5");
-			break;
-		case 6: 
-			hole = document.getElementById("hole6");
-			break;
-		case 7: 
-			hole = document.getElementById("hole7");
-			break;
-		case 8: 
-			hole = document.getElementById("hole8");
-			break;
-		case 9: 
-			hole = document.getElementById("hole9");
-			break;
-	}
-	return hole;
+function getHole(index) {
+	return document.getElementById(`hole${index}`);
 }
 
